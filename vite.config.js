3e0c1aa38a134fd63d2e4dcf5b7fe40980577e5f
Diana <didiliche@gmail.com>
2023-08-09 23:42:18 +0100

    undo changes to gitignore

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/divinity-os-2-crafting-recipes/',
+  base: '/',
   plugins: [vue()],
 })
