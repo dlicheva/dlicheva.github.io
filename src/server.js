47dbb480795c37d77740352d0f230a0522c42511
Diana <didiliche@gmail.com>
2023-08-09 22:11:28 +0100

    added data and formatted it

--- src/server.js
+++ src/server.js
@@ -5,6 +5,19 @@ async function fetchData() {
             'content-type': 'application/json'
         }
     });
-    return await res.json()
+    const recipes = await res.json()
+
+   // reformat result
+    return recipes.map((recipe) => {
+
+        // assign ingredients to array
+        let ingredients = [recipe['Ingredient-1'],recipe['Ingredient-2'], recipe['Ingredient-3'], recipe['Ingredient-4'], recipe['Ingredient-5']]
+        ingredients = ingredients.filter(ingredient => ingredient !== "")
+        return {
+                result: recipe['Result'],
+                description: recipe['Description'],
+                ingredients: ingredients
+            }
+    })
 }
 export default fetchData
